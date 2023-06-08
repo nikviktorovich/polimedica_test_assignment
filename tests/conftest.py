2d73855d5a4f0e92419934069c3327e181fcffe6
lnfecteDru <nikitasmn98@gmail.com>
2023-06-08 16:43:38 +0300

    Fixed wrong fixtures scope

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,13 +7,13 @@ import pytest
 import university.apps.fastapi_app.main
 
 
-@pytest.fixture
+@pytest.fixture(scope='function')
 def fastapi_app() -> Iterator[fastapi.FastAPI]:
     yield university.apps.fastapi_app.main.app
     university.apps.fastapi_app.main.app.dependency_overrides.clear()
 
 
-@pytest.fixture
+@pytest.fixture(scope='function')
 def test_client(fastapi_app) -> Iterator[fastapi.testclient.TestClient]:
     client = fastapi.testclient.TestClient(app=fastapi_app)
     with client:
