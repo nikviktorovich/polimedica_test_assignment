f748d7cfd2dfff95edc298126c8c2140cedcaeb4
lnfecteDru <nikitasmn98@gmail.com>
2023-06-08 15:41:02 +0300

    Added fastapi app

---
+++ src/university/apps/fastapi_app/main.py
@@ -0,0 +1,32 @@
+import logging
+
+import fastapi
+import fastapi.responses
+from fastapi import status
+
+import university.common.errors
+
+
+logger = logging.getLogger(__name__)
+app = fastapi.FastAPI()
+
+
+@app.exception_handler(university.common.errors.EntityNotFoundError)
+def not_found_error_handler(
+    request: fastapi.Request,
+    exception: university.common.errors.EntityNotFoundError,
+):
+    return fastapi.responses.JSONResponse(
+        content=str(exception),
+        status_code=status.HTTP_404_NOT_FOUND,
+    )
+
+
+@app.exception_handler(Exception)
+def global_exception_handler(request: fastapi.Request, exception: Exception):
+    logger.error(str(exception), exc_info=True)
+    return fastapi.responses.JSONResponse(
+        content='Server error',
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    )
+
