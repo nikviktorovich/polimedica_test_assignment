e2ac6f7561825213ed2a436042851d0ac94c9565
lnfecteDru <nikitasmn98@gmail.com>
2023-06-09 17:15:40 +0300

    Fixed fastapi lifespan not being called on tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,13 +7,14 @@ import pytest
 import university.apps.fastapi_app.main
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='session')
 def fastapi_app() -> Iterator[fastapi.FastAPI]:
     yield university.apps.fastapi_app.main.app
     university.apps.fastapi_app.main.app.dependency_overrides.clear()
 
 
-@pytest.fixture(scope='function')
-def test_client(fastapi_app) -> fastapi.testclient.TestClient:
+@pytest.fixture(scope='session')
+def test_client(fastapi_app) -> Iterator[fastapi.testclient.TestClient]:
     client = fastapi.testclient.TestClient(app=fastapi_app)
-    return client
+    with client:
+        yield client
